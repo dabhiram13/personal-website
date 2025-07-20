fd6781cc259512d81635a8df4958aa49c810e699
Wali <humaidwali20@gmail.com>
2025-07-20 11:41:38 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -352,14 +352,14 @@ export default function Personal() {
                         </span>
                       )}
                     </div>
-                    <a
+                    {/* <a
                       className="font-base group relative inline-block font-[450] text-zinc-900 dark:text-zinc-50"
                       href={design.link}
                       target="_blank"
                     >
                       View <LaunchIcon fontSize="small" />
                       <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 dark:bg-zinc-50 transition-all duration-200 group-hover:max-w-full"></span>
-                    </a>
+                    </a> */}
                     <p className="text-base text-zinc-600 dark:text-zinc-400 mt-2">
                       {design.description}
                     </p>
